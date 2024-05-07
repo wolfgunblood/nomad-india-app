7e5dd30deba2c3cee2d5fa7bfed83f86e7016435
wolfgunblood <jalajdorai1@gmail.com>
2024-05-07 13:19:33 +0530

    Done with the welcome page

--- app/index.js
+++ app/index.js
@@ -1,11 +1,59 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Image, Pressable, StyleSheet, Text, View } from 'react-native';
+import { wp, hp } from "../helpers/common";
+import { LinearGradient } from 'expo-linear-gradient';
+import Animated, { FadeInDown } from 'react-native-reanimated';
+import { theme } from '../constants/theme';
+import { useRouter } from 'expo-router';
 
 export default function App() {
+  const router = useRouter();
   return (
     <View style={styles.container}>
-      <Text>Hello World!!</Text>
-      <StatusBar style="auto" />
+      <StatusBar style='light' />
+
+      {/* <Image
+        source={require("../assets/images/welcome.png")}
+        style={styles.bgImage}
+        resizeMode='cover'
+      /> */}
+      {/* linear gradient  */}
+      <Animated.View entering={FadeInDown.duration(600)} style={{ flex: 1 }}>
+        <LinearGradient
+          colors={['rgba(255,255,255,0)', 'rgba(255,255,255,0.5)', 'white', 'white']}
+          style={styles.gradient}
+          start={{ x: 0.5, y: 0 }}
+          end={{ x: 0.5, y: 0.5 }}
+        />
+        {/* content */}
+        <View style={styles.contentContainer}>
+          <Animated.Text
+            entering={FadeInDown.delay(400).springify()}
+            style={styles.title}
+          >
+            Nomad
+          </Animated.Text>
+          <Animated.Text
+            entering={FadeInDown.delay(500).springify()}
+            style={styles.punching}
+          >
+            Every Place Tells a Story
+          </Animated.Text>
+          <Animated.View
+            entering={FadeInDown.delay(600).springify()}
+
+          >
+            <Pressable 
+              style={styles.startButton}
+              onPress={() => router.push("/home")}
+            >
+              <Text style={styles.startText}>Start Explore</Text>
+            </Pressable>
+          </Animated.View>
+        </View>
+      </Animated.View>
+
+
     </View>
   );
 }
@@ -13,8 +61,50 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    // backgroundColor: '#fff',
+    // alignItems: 'center',
+    // justifyContent: 'center',
   },
+  bgImage: {
+    width: wp(100),
+    height: hp(100),
+    position: "absolute"
+  },
+  gradient: {
+    width: wp(100),
+    height: hp(65),
+    bottom: 0,
+    position: "absolute"
+  },
+  contentContainer: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'flex-end',
+    gap: 14,
+  },
+  title: {
+    fontSize: hp(7),
+    color: theme.colors.neutral(0.9),
+    fontWeight: theme.fontWeights.bold
+  },
+  punching: {
+    fontSize: hp(2),
+    letterSpacing: 1,
+    marginBottom: 10,
+    fontWeight: theme.fontWeights.medium
+  },
+  startButton: {
+    marginBottom: 50,
+    backgroundColor: theme.colors.neutral(0.9),
+    padding: 15,
+    paddingHorizontal: 90,
+    borderRadius: theme.radius.xl,
+    borderCurve: 'continuous'
+  },
+  startText: {
+    color: theme.colors.white,
+    fontSize: hp(3),
+    fontWeight: theme.fontWeights.medium,
+    letterSpacing: 1
+  }
 });
